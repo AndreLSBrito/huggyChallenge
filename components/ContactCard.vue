4723da4e58dde5aa2cf216bbfcf505dc18aedfe0
Tiotedd <andrelsbrito2022@gmail.com>
2023-07-31 23:34:27 -0300

    first commit

---
+++ components/ContactCard.vue
@@ -0,0 +1,52 @@
+<template>
+  <section class="card">
+    <img/>
+
+    <secttion class="contact">
+      <h2>Augustinho Carrara</h2>
+
+      <div class="message">199,99 asdasdasd asdasda aasdas  sadas asd asdas asd asdsdasdasdsad asdasd asdas dsadsad sadaasd asdas asd a</div>
+    </secttion>
+  </section>
+</template>
+
+<style>
+  .card {
+    width: 100%;
+    display: flex;
+    height: 5.375rem;
+    padding: 0rem 0.5rem;
+    align-items: center;
+    gap: 0.75rem;
+    border-radius: 0.25rem;
+    background:  #EEF0FC;
+  }
+
+  .contact {
+    display: flex;
+    width: 100%;
+    height: 1.8125rem;
+    flex-direction: column;
+    justify-content: flex-end;
+    
+  }
+
+  h2 {
+    color: #2934A5;
+    font-family: IBM Plex Sans;
+    font-size: 1rem;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 1.6rem 
+  }
+
+  .message {
+    color: #46464A;
+    width: 100px;
+    white-space: nowrap;
+    /* overflow: hidden; */
+    text-overflow: ellipsis;
+    font-size: 0.875rem;
+    line-height: 1.575rem 
+  }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,65 @@
+<template>
+  <section class="container">
+    <aside class="inBox">
+      <section class="headerInBox">
+        <h1 >Minhas Mensagens</h1>     
+      </section>
+      
+      <ContactCard/>
+    </aside>
+    <main class="chat">
+      bbbbb
+    </main>
+  </section>
+</template>
+
+<script>
+  // import colors from "../utils/colors";
+
+  // export default {
+  //   data(){
+  //     return {
+  //       colors: colors
+  //     }
+  //   }
+  // }
+</script>
+
+<style>
+
+*{
+  margin: 0;
+  padding: 0;
+}
+
+.container{
+  display: flex;
+  height: 100vh;
+}
+
+.inBox{
+  margin: 0 0.5rem;
+  flex:1;
+}
+
+.headerInBox{
+  margin: 0.88rem 0;
+  padding: 0 1rem;
+  align-items: center;
+}
+
+h1{
+  color: #2934A5;
+  font-size: 1.25rem;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 180%; /* 2.25rem */
+}
+
+.chat{
+  flex: 2;
+  background-color: blueviolet;
+}
+
+</style>
+
