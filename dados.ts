202b4b657a945584d10e6edb7356fc4e7b3b5390
Tiotedd <andrelsbrito2022@gmail.com>
2023-08-02 22:45:59 -0300

    feat: create the chat page

---
+++ dados.ts
@@ -0,0 +1,59 @@
+export const chats = [
+  {
+    "name" : "Agustinho Carrara",
+    "messages": [
+      {
+        "hour": "10:35",
+        "content": "199,99 sadsadsadsa asdasasdasdas asdasdas asd asd as asd asdsa",
+        "user": false
+      },
+      {
+        "hour": "10:35",
+        "content": "199,99 sadsadsadsa asdasasdasdas asdasdas asd asd as asd asdsa",
+        "user": false
+      },
+      {
+        "hour": "10:35",
+        "content": "199,99 sadsadsadsa asdasasdasdas asdasdas asd asd as asd asdsa",
+        "user": false
+      },
+      {
+        "hour": "10:35",
+        "content": "199,99 sadsadsadsa asdasasdasdas asdasdas asd asd as asd asdsa",
+        "user": false
+      },
+      {
+        "hour": "10:35",
+        "content": "199,99 sadsadsadsa asdasasdasdas asdasdas asd asd as asd asdsa",
+        "user": false
+      },
+      {
+        "hour": "10:35",
+        "content": "199,99 sadsadsadsa asdasasdasdas asdasdas asd asd as asd asdsa",
+        "user": false
+      },
+      {
+        "hour": "10:35",
+        "content": "199,99 sadsadsadsa asdasasdasdas asdasdas asd asd as asd asdsa",
+        "user": false
+      },
+      {
+        "hour": "10:35",
+        "content": "199,99 sadsadsadsa asdasasdasdas asdasdas asd asd as asd asdsa",
+        "user": false
+      },
+      {
+        "hour": "10:35",
+        "content": "199,99 sadsadsadsa asdasasdasdas asdasdas asd asd as asd asdsa",
+        "user": true
+      }
+    ]
+  },
+  {
+    "name" : "Beiçola"
+  },
+  {
+    "name" : "Lineu Silva"
+  }
+]
+
--- pages/index.vue
+++ pages/index.vue
@@ -5,25 +5,75 @@
         <h1 >Minhas Mensagens</h1>     
       </section>
       
-      <ContactCard/>
-      <ContactCard/>
+      <ContactCard
+        v-for="(chat, indexChat) in chats"
+        @click="activeChat = indexChat"
+        :contact="chat.name"
+        :lastMessage="'199,99 asdasdsadasdasdasdsadsdasdsdasdsadasdasddasd'"
+        v-bind:key="indexChat"
+      />
+     
     </aside>
     <main class="chat">
-      bbbbb
+      <HeaderSection
+        :contactName="chats[activeChat].name"
+      />
+     
+      <article class="chatList">
+        <Message
+          v-for="(message, indexMessage) in chats[activeChat].messages"
+          v-bind:key="indexMessage"
+          :content="message.content"
+          :hour="message.hour"
+          :user="message.user"
+        />
+        <section>
+          <input
+            type="text"
+            class="input"
+            placeholder="Escreva sua mensagem"
+            v-model="contentNewMessage"
+            v-on:keyup.enter="sentMessage"
+          />
+        </section>
+      </article>
     </main>
   </section>
 </template>
 
 <script>
-  // import colors from "../utils/colors";
+  import { chats } from '../dados';
+  
+  export default {
+    data: function(){
+      return {
+        chats: chats,
+        activeChat: 0,
+        contentNewMessage: ""
+      }
+    },
 
-  // export default {
-  //   data(){
-  //     return {
-  //       colors: colors
-  //     }
-  //   }
-  // }
+    methods: {
+      sentMessage: function(){
+        let currentTime = new Date().getHours() +":" + new Date().getMinutes();
+
+        let newMessage = {
+          "hour": currentTime,
+          "content": this.contentNewMessage,
+          "user": true
+        };
+
+        this.chats[this.activeChat]
+          .messages
+          .push(newMessage)
+        ;
+
+        this.contentNewMessage="";
+
+        
+      }
+    }
+  }
 </script>
 
 <style>
@@ -33,9 +83,9 @@
   padding: 0;
 }
 
+
 .mainPage{
   display: flex;
-  height: 100vh;
 }
 
 .inBox{
@@ -52,15 +102,25 @@
 h1{
   color: #2934A5;
   font-size: 1.25rem;
-  font-style: normal;
   font-weight: 500;
   line-height: 180%; /* 2.25rem */
 }
 
 .chat{
-  flex: 2;
-  background-color: blueviolet;
+  flex: 3;
+  background-color: #F6F6F8;
+  height: 100vh;
+  overflow: auto;
+  
 }
+
+.chatList{
+  margin-top: 2.5rem;
+  padding: 1.5rem 1rem;
+  justify-content: flex-end;
+  flex-direction: column;
+}
+
 
 </style>
 
