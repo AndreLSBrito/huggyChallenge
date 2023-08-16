6f91d78d0b5ed4132a37b50d3b9a0eae1a51eb7e
Tiotedd <andrelsbrito2022@gmail.com>
2023-08-16 01:01:47 -0300

    fix: add proxy to resolve cors problems

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import express from 'express';
+const app = express();
+
 export default defineNuxtConfig({
   devtools: { enabled: true }
 })
+
+
